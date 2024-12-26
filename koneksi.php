8d3615605fb7e9dccfc2a6a341823ed25c6fd5be
tikameisya <tikaananda52@gmail.com>
2024-12-26 21:28:07 +0800

    Edit koneksi.php add localdev config

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,12 @@
 <?php
 
+//set this true if its local dev
+$localdev = false; //kena bila handak local dev di vs code set ini jadi true
+
 $_SERVER = "localhost";
-$_user = "root";
-$_password = "";
-$nama_database = "mochi"; 
+$_user = $localdev ? "root" : "meys_mochi";
+$_password = $localdev ? "" : "zP5M@ep-8^MzJocw";
+$nama_database = $localdev ? "mochi" : "meys_mochi"; 
 
 $db = mysqli_connect($_SERVER, $_user, $_password, $nama_database);
 
